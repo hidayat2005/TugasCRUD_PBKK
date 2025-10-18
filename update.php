eba2cdf3f4f537a55fffb0da7452440c983dba93
AL FIKRI HIDAYAT <hidayat05fikri@gmail.com>
2025-10-18 14:52:13 +0700

    update

--- update.php
+++ update.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $name = $_POST['name'];
     $email = $_POST['email'];
 
-    $sql = "UPDATE users SET name='$same', email='$email' WHERE id=$id";
+    $sql = "UPDATE users SET name='$name', email='$email' WHERE id=$id";
 
     if ($conn->query($sql) === TRUE){
         echo "Berhasil update data user!";
